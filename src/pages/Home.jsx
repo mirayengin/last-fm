50289d4c9972dc1fb12a89354554dab8c17981b6
erhan <erhanserkansabriye@gmail.com>
2022-12-21 13:09:47 +0300

    feat: proje bitti ama css var

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,49 @@
 import React, { useEffect, useState } from "react";
 import ArtistCard from "../components/Cards/ArtistCard";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchStart, fetchSuccess, fetchError } from "../features/FetchSlice";
-
+import { useDispatch } from "react-redux";
+import { fetchError } from "../features/FetchSlice";
 const Home = () => {
-  const [pageParam, setPageParam] = useState(2)
-  // const [loading, setLoading] = useState(true)
-  const {artist, loading} = useSelector((state)=>state.lastfm)
+  const [pageParam, setPageParam] = useState(1);
+  const [artistState, setArtistState] = useState([]);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const API_KEY = process.env.REACT_APP_API_KEY;
-  // const url = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${API_KEY}&format=json`;
-  const url = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${API_KEY}&page=${pageParam}&limit=5&format=json`;
-
+  const url = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${API_KEY}&page=${pageParam}&limit=10&format=json`;
   const getTopArtist = async () => {
-    dispatch(fetchStart());
+    setLoading(true);
     try {
       const { data } = await axios(url);
-      const {artists : {artist}} = data
-      dispatch(fetchSuccess(artist));
+      const {
+        artists: { artist },
+      } = data;
+      if (pageParam === 1) {
+        setArtistState([...artistState, ...artist.slice(0, 10)]);
+        //console.log(artist.slice(0, 5));
+        setLoading(false);
+      } else {
+        setArtistState([
+          ...artistState,
+          ...artist.slice((pageParam - 1) * 10, pageParam * 10),
+        ]);
+        setLoading(false);
+        if (pageParam === 6) {
+          setPageParam(1);
+        }
+      }
+      console.log(artistState);
     } catch (error) {
       console.log(error);
+      setLoading(false);
       dispatch(fetchError());
     }
   };
-
-  // useEffect(() => {
-  //   getTopArtist();
-  //   // eslint-disable-next-line
-  // }, []);
-
-  // const getTopArtist = async () => {
-  //   dispatch(fetchStart());
-  //   try {
-  //     const { data } = await axios(url);
-  //     const {
-  //       artists: { artist },
-  //     } = data;
-  //     console.log(artist);
-  //     dispatch(fetchSuccess(artist));
-  //   } catch (error) {
-  //     console.log(error);
-  //     dispatch(fetchError());
-  //   }
-  // };
   const handleScroll = () => {
-    // console.log("heigth:", document.documentElement.scrollHeight);
-    // console.log("top:", document.documentElement.scrollTop);
-    // console.log(window.innerHeight);
     if (
       window.innerHeight + document.documentElement.scrollTop + 1 >=
       document.documentElement.scrollHeight
     ) {
-      dispatch(fetchStart());
-      pageParam < 11 && setPageParam((prev) => prev + 1);
+      setPageParam((prev) => prev + 1);
     }
   };
   console.log(pageParam);
@@ -64,18 +54,16 @@ const Home = () => {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-    // eslint-disable-next-line
+    //   // eslint-disable-next-line
   }, []);
-
   return (
     <div>
       <h1>Artist List</h1>
-      {artist?.map((item,index) => (
+      {artistState?.map((item, index) => (
         <ArtistCard key={index} {...item} />
       ))}
-      {loading && <h2>LOADİNG! PLEASE WAİT</h2>}
+      {loading && <h2>loading</h2>}
     </div>
   );
 };
-
 export default Home;
