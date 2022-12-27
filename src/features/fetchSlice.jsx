f4fff6c84b0b1a34c7da3a5fba0f65a1e5433edd
erhan <erhanserkansabriye@gmail.com>
2022-12-27 13:45:00 +0300

    feat: redux düzenlendi

--- src/features/fetchSlice.jsx
+++ src/features/fetchSlice.jsx
@@ -1,8 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  artist: [],
-  loading: false,
+
   error: false,
   darkMode:false
 };
@@ -11,18 +10,10 @@ const FetchSlice = createSlice({
   name: "lastfm",
   initialState,
   reducers: {
-    fetchStart: (state, { payload }) => {
-      state.loading = true;
-    },
-    fetchSuccess: (state, { payload }) => {
-      state.loading = false;
-      state.artist = payload;
-      // console.log(payload);
-      // içine ne geldi kontrol etmek için
-    },
+ 
     fetchError: (state, { payload }) => {
       state.error = true;
-      state.loading = false;
+    
     },
     setDarkMode: (state, { payload }) => {
       state.darkMode = payload;
@@ -31,6 +22,6 @@ const FetchSlice = createSlice({
   },
 });
 
-export const { fetchStart, fetchSuccess, fetchError, setDarkMode } = FetchSlice.actions;
+export const {fetchError, setDarkMode } = FetchSlice.actions;
 
 export default FetchSlice.reducer;
